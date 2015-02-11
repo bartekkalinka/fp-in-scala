4404c6866b923a50b47889b8b5c2207a9ac471e1
Bartek Kalinka <bartkali@hotmail.com>
2015-02-11 18:14:41 +0100

    4.1 without orElse

--- src/main/scala/Ch4.scala
+++ src/main/scala/Ch4.scala
@@ -9,12 +9,19 @@ trait Option[+A] {
     case None => None
     case Some(a) => Some(f(a))
   }
-/*
-  def flatMap[B](f: A => Option[B]): Option[B]
-  def getOrElse[B >: A](default: => B): B
-  def orElse[B >: A](ob: => Option[B]): Option[B]
-  def filter(f: A => Boolean): Option[A]
-*/
+
+  def getOrElse[B >: A](default: => B): B = this match {
+    case None => default
+    case Some(a) => a
+  }
+
+  def flatMap[B](f: A => Option[B]): Option[B] = map[Option[B]](f).getOrElse(None)
+
+  def filter(f: A => Boolean): Option[A] = if(map[Boolean](f).getOrElse(false)) this else None
+
+  def filter2(f: A => Boolean): Option[A] = flatMap({a: A => if(f(a)) Some(a) else None})
+
+  //def orElse[B >: A](ob: => Option[B]): Option[B]
 }
 
 case class Some[+A](get: A) extends Option[A]
--- src/test/scala/Ch4Tests.scala
+++ src/test/scala/Ch4Tests.scala
@@ -9,10 +9,50 @@ class Chapter4Spec extends FlatSpec with Matchers {
   //import Chapter4._
 
   def getIfEven(i: Int) = {if(i % 2 == 0) Some(i) else None}
+  def getIfOdd(i: Int) = {if(i % 2 == 1) Some(i) else None}
 
   "map" should "transform optional element" in {
     getIfEven(2).map {_.toString} should be (Some("2"))
     getIfEven(1).map {_.toString} should be (None)
+  }
+  "getOrElse" should "return the result inside the Some case of the Option" in {
+    getIfEven(2).getOrElse(5) should be (2)
+  }
+
+  it should "return the given default value if the Option is None" in {
+    getIfEven(1).getOrElse(5) should be (5)
+  }
+
+/*
+  "orElse" should "return the first Option if it’s defined" in {
+    getIfEven(2).orElse(getIfEven(1)) should be (Some(2))
+  }
+
+  it should "it return the second Option otherwise" in {
+    getIfEven(1).orElse(getIfEven(2)) should be (Some(2))
+    getIfEven(1).orElse(getIfEven(5)) should be (None)
+  }
+*/
+
+  "flatMap" should "transform optional element with function giving optional result" in {
+    getIfEven(2).flatMap {getIfEven(_)} should be (Some(2))
+    getIfEven(2).flatMap {getIfOdd(_)} should be (None)
+    getIfEven(1).flatMap {getIfOdd(_)} should be (None)
+    getIfOdd(1).flatMap {getIfOdd(_)} should be (Some(1))
+  }
+
+  "filter" should "express logic value in terms of option" in {
+    getIfEven(2).filter(_ % 2 == 0) should be (Some(2))
+    getIfEven(1).filter(_ % 2 == 0) should be (None)
+    getIfOdd(1).filter(_ % 2 == 0) should be (None)
+    getIfOdd(1).filter(_ % 2 == 1) should be (Some(1))
+  }
+
+  "filter2" should "express logic value in terms of option" in {
+    getIfEven(2).filter2(_ % 2 == 0) should be (Some(2))
+    getIfEven(1).filter2(_ % 2 == 0) should be (None)
+    getIfOdd(1).filter2(_ % 2 == 0) should be (None)
+    getIfOdd(1).filter2(_ % 2 == 1) should be (Some(1))
   }
 
 }
