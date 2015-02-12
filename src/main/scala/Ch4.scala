3b20b35efdb770053ea362932d51bf3582f16d18
Bartek Kalinka <bartkali@hotmail.com>
2015-02-12 12:20:03 +0100

    to 4.4

--- src/main/scala/Ch4.scala
+++ src/main/scala/Ch4.scala
@@ -21,9 +21,43 @@ trait Option[+A] {
 
   def filter2(f: A => Boolean): Option[A] = flatMap({a: A => if(f(a)) Some(a) else None})
 
-  //def orElse[B >: A](ob: => Option[B]): Option[B]
+  def orElse[B >: A](ob: => Option[B]): Option[B]
 }
 
-case class Some[+A](get: A) extends Option[A]
-case object None extends Option[Nothing]
+case class Some[+A](get: A) extends Option[A] {
+  override def orElse[B >: A](ob: => Option[B]): Option[B] = this
+}
+case object None extends Option[Nothing] {
+  override def orElse[B >: Nothing](ob: => Option[B]): Option[B] = ob
+}
+
+object Chapter4 {
+
+  import Chapter3._
+
+  //4.2
+  def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+
+  def variance(xs: Seq[Double]): Option[Double] = mean(xs).flatMap({m => mean(xs.map({x => math.pow(x - m, 2)}))})
+
+  //4.3
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = (a, b) match {
+    case (Some(x), Some(y)) => Some(f(x, y))
+    case _ => None
+  }
+
+  //4.4
+  def sequence[A](a: List[Option[A]]): Option[List[A]] =
+    foldLeft[Option[A], Option[List[A]]](a, Some(Nil))({
+        case (Some(_acc), Some(k)) => Some(Cons(k, _acc))
+        case _ => None
+      }).map(reverseFold[A])
+
+  //4.5
+  //TODO
+  //def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]]
+
+}
 
--- src/test/scala/Ch4Tests.scala
+++ src/test/scala/Ch4Tests.scala
@@ -6,7 +6,7 @@ import fp._
 
 class Chapter4Spec extends FlatSpec with Matchers {
 
-  //import Chapter4._
+  import Chapter4._
 
   def getIfEven(i: Int) = {if(i % 2 == 0) Some(i) else None}
   def getIfOdd(i: Int) = {if(i % 2 == 1) Some(i) else None}
@@ -23,7 +23,6 @@ class Chapter4Spec extends FlatSpec with Matchers {
     getIfEven(1).getOrElse(5) should be (5)
   }
 
-/*
   "orElse" should "return the first Option if it’s defined" in {
     getIfEven(2).orElse(getIfEven(1)) should be (Some(2))
   }
@@ -32,7 +31,6 @@ class Chapter4Spec extends FlatSpec with Matchers {
     getIfEven(1).orElse(getIfEven(2)) should be (Some(2))
     getIfEven(1).orElse(getIfEven(5)) should be (None)
   }
-*/
 
   "flatMap" should "transform optional element with function giving optional result" in {
     getIfEven(2).flatMap {getIfEven(_)} should be (Some(2))
@@ -53,6 +51,23 @@ class Chapter4Spec extends FlatSpec with Matchers {
     getIfEven(1).filter2(_ % 2 == 0) should be (None)
     getIfOdd(1).filter2(_ % 2 == 0) should be (None)
     getIfOdd(1).filter2(_ % 2 == 1) should be (Some(1))
+  }
+
+  "variance" should "get variance right" in {
+    variance(Seq()) should be (None)
+    variance(Seq(0.0, 3.0, 6.0)) should be (Some(6.0))
+  }
+
+  "map2" should "map 2 options with 1 function" in {
+    map2(getIfEven(2), getIfOdd(1))(_ + _) should be (Some(3))
+    map2(getIfEven(2), getIfEven(1))(_ + _) should be (None)
+    map2(getIfOdd(2), getIfOdd(1))(_ + _) should be (None)
+  }
+
+  "sequence" should "push option outside of list" in {
+    sequence(List(Some(1), Some(2), Some(3))) should be (Some(List(1, 2, 3)))
+    sequence(List(Some(1), None, Some(3))) should be (None)
+    sequence(List()) should be (Some(List()))
   }
 
 }
