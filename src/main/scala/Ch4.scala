df0f34b35c633241f68b421e9405c1d467345ac6
Bartek Kalinka <bartkali@hotmail.com>
2015-02-13 14:50:39 +0100

    to 4.6

--- src/main/scala/Ch4.scala
+++ src/main/scala/Ch4.scala
@@ -56,8 +56,41 @@ object Chapter4 {
       }).map(reverseFold[A])
 
   //4.5
-  //TODO
-  //def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]]
+  def Try[A](a: => A): Option[A] =
+    try Some(a)
+    catch { case e: Exception => None }
+
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
+    foldLeft[A, Option[List[B]]](a, Some(Nil))((acc, k) =>
+      (acc, f(k)) match {
+        case (Some(_acc), Some(l)) => Some(Cons(l, _acc))
+        case _ => None
+      }
+    ).map(reverseFold[B])
+
+  def sequence2[A](a: List[Option[A]]): Option[List[A]] = traverse[Option[A], A](a)({opt => opt})
+
+  //4.6
+  trait Either[+E, +A] {
+    def map[B](f: A => B): Either[E, B]
+    def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B]
+    def orElse[EE >: E,B >: A](b: => Either[EE, B]): Either[EE, B]
+    def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C):Either[EE, C]
+  }
+
+  case class Left[+E](value: E) extends Either[E, Nothing] {
+    def map[B](f: Nothing => B): Either[E, B] = Left(value)
+    def flatMap[EE >: E, B](f: Nothing => Either[EE, B]): Either[EE, B] = Left(value)
+    def orElse[EE >: E,B >: Nothing](b: => Either[EE, B]): Either[EE, B] = b
+    def map2[EE >: E, B, C](b: Either[EE, B])(f: (Nothing, B) => C):Either[EE, C] = Left(value)
+  }
+
+  case class Right[+A](value: A) extends Either[Nothing, A] {
+    def map[B](f: A => B): Either[Nothing, B] = Right(f(value))
+    def flatMap[EE >: Nothing, B](f: A => Either[EE, B]): Either[EE, B] = f(value)
+    def orElse[EE >: Nothing,B >: A](b: => Either[EE, B]): Either[EE, B] = Right(value)
+    def map2[EE >: Nothing, B, C](b: Either[EE, B])(f: (A, B) => C):Either[EE, C] = b.map[C]({bb: B => f(value, bb)})
+  }
 
 }
 
--- src/test/scala/Ch4Tests.scala
+++ src/test/scala/Ch4Tests.scala
@@ -64,10 +64,54 @@ class Chapter4Spec extends FlatSpec with Matchers {
     map2(getIfOdd(2), getIfOdd(1))(_ + _) should be (None)
   }
 
+  def correctSequence(seq: List[Option[Int]] => Option[List[Int]]) = {
+    seq(List(Some(1), Some(2), Some(3))) should be (Some(List(1, 2, 3)))
+    seq(List(Some(1), None, Some(3))) should be (None)
+    seq(List()) should be (Some(List()))
+  }
+
   "sequence" should "push option outside of list" in {
-    sequence(List(Some(1), Some(2), Some(3))) should be (Some(List(1, 2, 3)))
-    sequence(List(Some(1), None, Some(3))) should be (None)
-    sequence(List()) should be (Some(List()))
+    correctSequence(sequence[Int])
+  }
+
+  "sequence2" should "push option outside of list" in {
+    correctSequence(sequence2[Int])
+  }
+
+  "traverse" should "traverse list with function and push option outside of list" in {
+    traverse[String, Int](List("1", "2", "3"))({x => Try(x.toInt)}) should be (Some(List(1, 2, 3)))
+    traverse[String, Int](List("1", "2", "a"))({x => Try(x.toInt)}) should be (None)
+  }
+
+  def evenRight(i: Int) = {if(i % 2 == 0) Right(i) else Left("odd")}
+  def oddRight(i: Int) = {if(i % 2 == 1) Right(i) else Left("even")}
+
+  "map" should "transform alternative" in {
+    evenRight(2).map {_.toString} should be (Right("2"))
+    evenRight(1).map {_.toString} should be (Left("odd"))
+  }
+
+  "orElse" should "return first if it's right" in {
+    evenRight(2).orElse(evenRight(1)) should be (Right(2))
+  }
+
+  it should "it return the second otherwise" in {
+    evenRight(1).orElse(evenRight(2)) should be (Right(2))
+    evenRight(1).orElse(evenRight(5)) should be (Left("odd"))
+  }
+
+  "flatMap" should "transform alternative with function giving alternative result" in {
+    evenRight(2).flatMap {evenRight(_)} should be (Right(2))
+    evenRight(2).flatMap {oddRight(_)} should be (Left("even"))
+    evenRight(1).flatMap {evenRight(_)} should be (Left("odd"))
+    oddRight(1).flatMap {oddRight(_)} should be (Right(1))
+  }
+
+  "map2" should "map binary operator through alternatives" in {
+    evenRight(4).map2(evenRight(2))(_ + _) should be (Right(6))
+    evenRight(3).map2(evenRight(2))(_ + _) should be (Left("odd"))
+    oddRight(3).map2(oddRight(2))(_ + _) should be (Left("even"))
+    oddRight(3).map2(oddRight(5))(_ + _) should be (Right(8))
   }
 
 }
