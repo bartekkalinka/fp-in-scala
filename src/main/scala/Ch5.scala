d4d3c94a42fd0c34941b03ad1ed66b750559f0f7
Bartek Kalinka <bartkali@hotmail.com>
2015-02-14 22:41:45 +0100

    to 5.3

--- src/main/scala/Ch5.scala
+++ src/main/scala/Ch5.scala
@@ -4,19 +4,35 @@
 package fp
 
 object Chapter5 {
+  class CallForEmptyException extends Exception
+
   trait Stream[+A] {
     def headOption: Option[A]
 
     //5.1
     def toList: List[A]
+
+    //5.2
+    def take(n: Int): List[A]
+    def drop(n: Int): Stream[A]
+
+    //5.3
+    def takeWhile(p: A => Boolean): Stream[A]
  }
   case object Empty extends Stream[Nothing] {
     def headOption: Option[Nothing] = None
     def toList: List[Nothing] = Nil
+    def take(n: Int): List[Nothing] = {if(n == 0) Nil else throw new CallForEmptyException}
+    def drop(n: Int): Stream[Nothing] = {if(n == 0) this else throw new CallForEmptyException}
+    def takeWhile(p: Nothing => Boolean): Stream[Nothing] = this
   }
+
   case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A] {
     def headOption: Option[A] = Some(h())
     def toList: List[A] = h() :: t().toList
+    def take(n: Int): List[A] =  n match {case 0 => Nil; case m => h() :: t().take(m - 1)}
+    def drop(n: Int): Stream[A] = n match {case 0 => this; case m => t().drop(m - 1)}
+    def takeWhile(p: A => Boolean): Stream[A] = if(p(h())) Cons(h, () => t().takeWhile(p)) else Empty
   }
 
   object Stream {
@@ -28,6 +44,8 @@ object Chapter5 {
     def empty[A]: Stream[A] = Empty
     def apply[A](as: A*): Stream[A] =
       if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+
+
 
   }
 
--- src/test/scala/Ch5Tests.scala
+++ src/test/scala/Ch5Tests.scala
@@ -10,4 +10,21 @@ class Chapter5Spec extends FlatSpec with Matchers {
   "toList" should "force stream evaluation" in {
     Stream(1, 2, 3, 4, 5, 6, 7).toList should be (List(1, 2, 3, 4, 5, 6, 7))
   }
+
+  "take" should "force evaluation of first n elements" in {
+    Stream(1, 2, 3, 4, 5, 6, 7).take(2) should be (List(1, 2))
+    Stream(1, 2).take(2) should be (List(1, 2))
+  }
+
+  "drop" should "drop first n elements" in {
+    Stream(1, 2, 3, 4, 5, 6, 7).drop(2).toList should be (List(3, 4, 5, 6, 7))
+    Stream(1, 2).drop(2) should be (Empty)
+  }
+
+  "takeWhile" should "take all elements that match" in {
+    Stream(1, 3, 5, 8, 9, 11).takeWhile(_ % 2 == 1).toList should be (List(1, 3, 5))
+    Stream(2, 1, 3).takeWhile(_ % 2 == 1) should be (Empty)
+    Stream[Int]().takeWhile(_ % 2 == 1) should be (Empty)
+  }
+
 }
