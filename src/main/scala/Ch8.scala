df4962dd41d9fca85d2ecdcf05354f679205f7c5
Bartek Kalinka <bartkali@hotmail.com>
2015-03-20 17:46:39 +0100

    8.5

--- src/main/scala/Ch8.scala
+++ src/main/scala/Ch8.scala
@@ -5,7 +5,7 @@
 package fp
 
 object Chapter8 {
-  import Chapter6.{State, RNG, Rand}
+  import Chapter6.{State, RNG}
 
   case class Gen[A](sample: State[RNG,A])
 
@@ -22,10 +22,12 @@ object Chapter8 {
 
     //8.4
 
+    //cheat sheet:
     //type Rand[+A] = RNG => (A, RNG)
     //def nonNegativeLessThan(n: Int): Rand[Int]
     //Rand[Int] is (RNG => (Int, RNG))
     //case class State[S,+A](run: S => (A,S))
+
     def choose(start: Int, stopExclusive: Int): Gen[Int] =
       Gen[Int](State[RNG, Int]({
         rng: RNG => {
@@ -33,6 +35,20 @@ object Chapter8 {
           (num + start, rngAfter)
         }
       }))
+
+    //8.5
+    def unit[A](a: => A): Gen[A] = Gen(State((a, _)))
+
+    def boolean: Gen[Boolean] = Gen(State({
+      rng =>
+        val (num, rng2) = choose(1, 11).sample.run(rng)
+        (num % 2 == 0, rng2)
+    }))
+
+    //def sequence[S, A](fs: List[State[S, A]]): State[S, List[A]]
+    def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] = Gen(
+      State.sequence[RNG, A](List.fill[State[RNG, A]](n)(g.sample))
+    )
   }
 
 }
--- src/test/scala/Ch8ScalaCheck.scala
+++ src/test/scala/Ch8ScalaCheck.scala
@@ -4,7 +4,7 @@
 package fp
 
 import org.scalacheck.{Gen, Properties}
-import org.scalacheck.Prop.{forAll, BooleanOperators}
+import org.scalacheck.Prop.{forAll, BooleanOperators, all}
 import scala.util.Random
 
 //8.1
@@ -48,5 +48,31 @@ object Ch8GenScalaCheckSpec extends Properties("Chapter8.Gen") {
       val res = Chapter8.Gen.choose(1, 10).sample.run(Chapter6.SimpleRNG(seed))._1
       (res >= 1) :| "result >= " &&
       (res < 10) :| "result < "
+  }
+
+  property("unit returns constant value") = forAll {
+    (seed: Long) =>
+      Chapter8.Gen.unit("a").sample.run(Chapter6.SimpleRNG(seed))._1 == "a"
+  }
+
+  property("listOfN return list of given length") = forAll(Gen.choose(1L, 1000000L), Gen.choose(0, 50)) {
+    (seed: Long, n: Int) =>
+      val res = Chapter8.Gen.listOfN(n, Chapter8.Gen.choose(1, 10)).sample.run(Chapter6.SimpleRNG(seed))._1
+      (res.length == n) :| "length" &&
+      (res.filter(_ >= 10).isEmpty) :| "range"
+  }
+
+  property("boolean gives equal distribution of true and false values") =
+    forAll(Gen.choose(1L, 1000000L), Gen.choose(500, 1000)) {
+    (seed: Long, n: Int) =>
+        val res: List[Boolean] = Chapter8.Gen.listOfN(n, Chapter8.Gen.boolean).sample.run(Chapter6.SimpleRNG(seed))._1
+        val trueCnt: Int = res.count(b => b)
+        val falseCnt = res.count(b => !b)
+        ("evidence " + n + " " + trueCnt + " " + falseCnt) |: all (
+          (trueCnt >= (n * 4 / 10)) :| "true >=",
+          (trueCnt < (n * 6 / 10)) :| "true <",
+          (falseCnt >= (n * 4 / 10)) :| "false >=",
+          (falseCnt < (n * 6 / 10)) :| "false <"
+        )
   }
 }
