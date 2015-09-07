217d2e9d429904df49fefc5e5e6ed7959d5adb1f
Bartek Kalinka <bartkali@hotmail.com>
2015-09-07 20:05:02 +0200

    example code from Ch14

---
+++ src/main/scala/Ch14.scala
@@ -0,0 +1,57 @@
+package fp
+
+object Chapter14 {
+  sealed trait ST[S,A] { self =>
+    protected def run(s: S): (A,S)
+    def map[B](f: A => B): ST[S,B] = new ST[S,B] {
+      def run(s: S) = {
+        val (a, s1) = self.run(s)
+        (f(a), s1)
+      }
+    }
+    def flatMap[B](f: A => ST[S,B]): ST[S,B] = new ST[S,B] {
+      def run(s: S) = {
+        val (a, s1) = self.run(s)
+        f(a).run(s1)
+      }
+    }
+  }
+
+  object ST {
+    def apply[S,A](a: => A) = {
+      lazy val memo = a
+      new ST[S,A] {
+        def run(s: S) = (memo, s)
+      }
+    }
+  }
+
+  sealed trait STRef[S,A] {
+    protected var cell: A
+    def read: ST[S,A] = ST(cell)
+    def write(a: A): ST[S,Unit] = new ST[S,Unit] {
+      def run(s: S) = {
+        cell = a
+        ((), s)
+      }
+    }
+  }
+  object STRef {
+    def apply[S,A](a: A): ST[S, STRef[S,A]] = ST(new STRef[S,A] {
+      var cell = a
+    })
+  }
+
+  def test1(i1: Int, i2: Int) = {
+    for {
+      r1 <- STRef[Nothing,Int](i1)
+      r2 <- STRef[Nothing,Int](i2)
+      x <- r1.read
+      y <- r2.read
+      _ <- r1.write(y+1)
+      _ <- r2.write(x+1)
+      a <- r1.read
+      b <- r2.read
+    } yield (a,b)
+  }
+}
